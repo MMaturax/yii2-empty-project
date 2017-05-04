abec1d2d125066861487cf57442701b656be5d85
maturax <abdullah@karipcin.com>
2017-05-04 13:28:38 +0300

    scss

--- _app/views/site/index.php
+++ _app/views/site/index.php
@@ -3,11 +3,20 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Url;
+use yii\widgets\Menu;
 
 $this->title = 'My Yii Application';
-
+//Url::to(['man/advert-create-global'])
 //var_dump(Url::base());
 //var_dump(Yii::getAlias('@web'));
 ?>
-
+<?= Menu::widget([
+    'encodeLabels' => false,
+    'options'=>['class'=>'left-nav'],
+    'items' => [
+        ['label' => 'ANASAYFA', 'url' => ['site/index']],
+        ['label' => 'ÜRÜNLER', 'url' => ['site/categories']],
+        ['label' => 'HABERLER', 'url' => ['site/news']],
+    ]
+]) ?>
 
