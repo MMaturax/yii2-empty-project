e3288622e17fcca5473e786303204d6e113042be
maturax <abdullah@karipcin.com>
2017-05-04 10:54:35 +0300

    scss

---
+++ assets/config.rb
@@ -0,0 +1,38 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'susy'
+require 'bootstrap-sass'
+# Set this to the root of your project when deployed:
+
+#http_images_dir = "imgxx"
+
+#http_path="http://test.com/assets/img"
+#http_images_path = http_path + "/" + images_dir
+http_images_path = "assets/img"
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+#config.action_controller.asset_host = "http://your.domain.com/"
+
+
+
